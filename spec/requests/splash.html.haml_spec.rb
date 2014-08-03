75f1fbc1ae094604408cbd1b4cd414e0d249e4ff
psytau <rmanninoFoo@gmail.com>
2014-08-03 14:05:46 -0700

    remove comming_soon controller and view tests [#76244132]

--- spec/requests/splash.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "comming_soon/splash.html.haml", :type => :view do
-  # render
-end
--- spec/views/comming_soon/splash.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "comming_soon/splash.html.haml", :type => :view do
-  it "should print out a splash screen" do
-    render
-    assert_select 'h1'
-  end
-end
