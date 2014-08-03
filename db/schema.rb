587be3a389756a04b3a05aa9bc6d52847121b4ac
psytau <rmanninoFoo@gmail.com>
2014-08-02 23:16:00 -0700

    chanaged order of another migration [Finishes #76235526]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803032250) do
+ActiveRecord::Schema.define(version: 20140803032251) do
 
   create_table "books", force: true do |t|
     t.string   "author"
