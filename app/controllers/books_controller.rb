1b349a3f5342487333c6a747a5ff7f07e7bbfb67
psytau <rmanninoFoo@gmail.com>
2014-08-04 06:24:51 -0700

    require a logged in user for book editing and creation [#75945284]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
@@ -71,4 +72,5 @@ class BooksController < ApplicationController
     def book_params
       params.require(:book).permit(:author, :title, :status, :isbn, :rating, :active, :approved_at)
     end
+
 end
