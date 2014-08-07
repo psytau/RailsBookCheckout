7205f3ebb63e6c15b87446cd9bb8f00213c5b326
John Wade <JohnRobertWade@gmail.com>
2014-08-07 10:43:52 -0700

    [#75987566] Added cloudinary and attachinary for image upload to books. Added photos to Book Controller. See PT for instruction on adding images to views.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,7 +70,7 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:author, :title, :status, :isbn, :rating, :active, :approved_at)
+      params.require(:book).permit(:photos, :author, :title, :status, :isbn, :rating, :active, :approved_at)
     end
 
 end
