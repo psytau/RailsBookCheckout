00ade2fe1ecef66954b46b5c541fa3207a4680bd
psytau <rmanninoFoo@gmail.com>
2014-08-18 18:15:31 -0700

    fixed controller spec [#77161738]

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe AdminController, :type => :controller do
 
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @admin = FactoryGirl.create :admin
+
+  end
+
   describe "GET users" do
-    it "returns http success" do
+    it "returns http success when logged in as admin" do
+      sign_in @admin
       get :users
       expect(response).to be_success
+    end
+    it "returns http success when logged in as admin" do
+      sign_in @user
+      expect {get :users}.to raise_exception(CanCan::AccessDenied)
     end
   end
 
