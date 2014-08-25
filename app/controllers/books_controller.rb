4a9782f90bf4d8ae4a2420cf026d57751cc6fb62
psytau <rmanninoFoo@gmail.com>
2014-08-25 15:57:58 +0800

    cancan permisions for books [#75987378]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,9 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    if cannot? :update, @book
+      redirect_to @book, notice: 'You cannot edit this book.'
+    end
   end
 
   # POST /books
@@ -53,12 +56,17 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1.json
   def update
     respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
+      if can? :update, @book
+        if @book.update(book_params)
+          format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+          format.json { render :show, status: :ok, location: @book }
+        else
+          format.html { render :edit }
+          format.json { render json: @book.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.html {redirect_to @book, notice: 'You cannot edit this book.'}
+        format.json { render json: 'permission denied', status: :unauthorize }
       end
     end
   end
@@ -66,10 +74,16 @@ class BooksController < ApplicationController
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+    if can? :destroy, @book
+      @book.destroy
+      respond_to do |format|
+        format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html {redirect_to @book, notice: 'You may not delete this book.'}
+      end
     end
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,11 @@ class Ability
   def initialize(user)
     if user.is_admin?  # use rolify
       can :approve, Book
+      can :manage, :all
+    else
+      can [:update, :destroy], Book, user_id: user.id
     end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
