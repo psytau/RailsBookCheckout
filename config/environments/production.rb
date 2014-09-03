b63818f178b87427e247b7d401ffd7af9861a13e
psytau <rmanninoFoo@gmail.com>
2014-09-04 02:08:32 +0800

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  o
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
