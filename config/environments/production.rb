185edf534ec5414e3acf346fc230a514cbfd6c9e
psytau <rmanninoFoo@gmail.com>
2014-09-06 01:49:39 +0800

    added host name to prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.default :from => 'no-reply@railsbookcheckout.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => "railsbookcheckout.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
