21bae6f4ba28406ef5b6bfc326638683f6d81b86
psytau <rmanninoFoo@gmail.com>
2014-09-21 17:23:15 +0800

    Created a test for follow messages #35

---
+++ spec/features/follow_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+
+feature "Follow" do
+
+  before(:each) do
+    @user = FactoryGirl.create :user
+    @book = FactoryGirl.create :book
+    @book.user = @user
+    @book.save!
+    login_as(@user, :scope => :user)
+  end
+
+  scenario "User follows a book" do
+    visit book_path @book
+    click_link 'Follow'
+
+    follower = Follower.where("user_id = ? and book_id = ?", @user.id, @book.id).first
+    expect(follower.id).to eq(@user.id)
+  end
+
+  scenario "User follows a book and others review that book" do
+    visit book_path @book
+    click_link 'Follow'
+    user2 = FactoryGirl.create :user
+    login_as(user2, :scope => :user)
+    visit book_path @book
+
+    fill_in "book_review_content",
+             :with => "This is a heartbreaking tale of breathtaking beauty."
+    click_button "Create Book review"
+    expect(page).to have_text("a heartbreaking tale")
+
+    login_as(@user, :scope => :user)
+
+    # now your message path should have a message about user2
+    # leaving a book review for @book
+    visit messages_path
+    expect(page).to have_text(user2.firstname)
+    expect(page).to have_text(@book.title)
+  end
+
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,11 +8,11 @@ require 'capybara/rspec'
 include Warden::Test::Helpers
 Warden.test_mode!
 
-Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, {debug: false})
-end
+# Capybara.register_driver :poltergeist do |app|
+#     Capybara::Poltergeist::Driver.new(app, {debug: false})
+# end
 
-Capybara.javascript_driver = :poltergeist
+# Capybara.javascript_driver = :poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
